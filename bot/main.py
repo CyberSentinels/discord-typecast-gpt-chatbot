729b898738c485819964c967ff47408f9a68273d
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-03-25 21:14:54 -0500

    Init rate limiting

--- bot/main.py
+++ bot/main.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 import discord
 import datetime
@@ -22,7 +23,6 @@ if not DISCORD_BOT_APP_TOKEN:
 intents = discord.Intents.all()
 intents.message_content = True
 client = commands.Bot(command_prefix=["!", "/"], intents=intents)
-
 
 @client.event
 async def on_ready():
@@ -55,9 +55,41 @@ async def on_ready():
     await client.change_presence(activity=activity, status=Status.online)
 
 
+# Add global dictionaries for tracking user and server question counts
+user_question_count = {}
+server_question_count = {}
+user_count_limit = 5
+server_count_limit = 100
+
 @client.event
 async def on_message(message):
+    global user_question_count, server_question_count
     if client.user.mentioned_in(message):
+        # Check if the user has admin permissions
+        author_perms = message.author.permissions_in(message.channel)
+        if not author_perms.administrator:
+            # Increment user and server question counts
+            user_question_count[message.author.id] = user_question_count.get(message.author.id, 0) + 1
+            server_question_count[message.guild.id] = server_question_count.get(message.guild.id, 0) + 1
+            userfailcondition = user_question_count[message.author.id] > user_count_limit
+            serverfailcondition = server_question_count[message.guild.id] > server_count_limit
+            if userfailcondition:
+                faildescription = f"You have reached the daily question limit of '{user_count_limit}' per day."
+            if serverfailcondition:
+                faildescription = f"The server reached the daily question limit of '{server_count_limit} per day."
+            failconditions = userfailcondition or serverfailcondition
+            # Check if the user or server has reached the question limit
+            if failconditions:
+                await message.channel.send(
+                    embed=discord.Embed(
+                        type="rich",
+                        title="Question limit reached",
+                        description=faildescription,
+                        color=discord.Colour.red(),
+                    )
+                )
+                return
+
         try:
             await handle_message(message)
         except Exception as e:
@@ -70,6 +102,24 @@ async def on_message(message):
                 )
             )
 
+# Define the loop
+@tasks.loop(hours=24)
+async def reset_question_counts():
+    global user_question_count, server_question_count
+    user_question_count = {}
+    server_question_count = {}
+    print("Question counts reset at", datetime.datetime.now())
+
+# Schedule the loop to start at the next midnight
+@reset_question_counts.before_loop
+async def before_reset_question_counts():
+    now = datetime.datetime.now()
+    midnight = now.replace(hour=0, minute=0, second=0, microsecond=0) + datetime.timedelta(days=1)
+    time_until_midnight = midnight - now
+    await asyncio.sleep(time_until_midnight.total_seconds())
+
+# Start the loop
+reset_question_counts.start()
 
 try:
     client.run(DISCORD_BOT_APP_TOKEN)
