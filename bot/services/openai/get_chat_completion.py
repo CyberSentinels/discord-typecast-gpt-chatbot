0c72dcaa77d872f2fcf8441b79a1ab5554a9f1c1
1nf053c <1nf053c>
2023-03-28 18:33:35 -0400

    increase bot resilience by using retry logic & exponential dropoff logic

--- bot/services/openai/get_chat_completion.py
+++ bot/services/openai/get_chat_completion.py
@@ -1,3 +1,7 @@
+import time
+from requests.exceptions import RequestException
+
+
 def create_chatml_messages(system_content, user_content):
     chatml_messages = [
         {
@@ -13,18 +17,26 @@ def create_chatml_messages(system_content, user_content):
 
 
 def get_chat_completion(openai, chatml_messages):
-    try:
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=chatml_messages,
-            max_tokens=256,
-            temperature=0.8,
-            top_p=1,
-            n=1,
-            presence_penalty=0.2,
-            frequency_penalty=0.2,
-        )
-        return response
-    except Exception as e:
-        print("error in get_chat_completion")
-        raise e
+    max_retries = 3
+    delay = 5
+    for retry in range(max_retries):
+        try:
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=chatml_messages,
+                max_tokens=256,
+                temperature=0.8,
+                top_p=1,
+                n=1,
+                presence_penalty=0.2,
+                frequency_penalty=0.2,
+            )
+            return response
+        except RequestException as e:
+            if retry == max_retries - 1:
+                print("Maximum retries exceeded. Raising exception.")
+                raise e
+            print(f"Request failed. Retrying in {delay} seconds...")
+            time.sleep(delay)
+            delay += 10
+    return None
